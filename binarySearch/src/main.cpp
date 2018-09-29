e9c9f20e653e6907190529d6100bb048a0316fe8
Tom Willis <tom.willis@live.com>
2018-09-29 18:25:58 +0100

    Fixing error in binarySearchWithIndex.

--- binarySearch/src/main.cpp
+++ binarySearch/src/main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <catch.hpp>
 
 bool binarySearch(int* array, int arrayLength, int value) {		
 	if(value == *(array+((arrayLength-1)/2)))
@@ -14,12 +13,29 @@ bool binarySearch(int* array, int arrayLength, int value) {
 		return binarySearch((array+((arrayLength-1)/2))+1,arrayLength-((arrayLength-1)/2)+1,value);
 }
 
+int binarySearchWithIndex(int* array, int arrayLength, int value, int index) {	
+	printf("Index: %d\n", index);
+	printf("ArrayLength: %d\n", index);
+	printf("Element 0: %d\n", *array);
+	if(value == *(array+((arrayLength-1)/2)))
+		return index+((arrayLength-1)/2);	
+	else if(arrayLength == 0)
+		return -1;
+	else if(value < *(array+((arrayLength-1)/2)))
+		return binarySearchWithIndex(array+((arrayLength-1)/2)-1,(arrayLength-1)/2,value,index);
+	else 
+		return binarySearchWithIndex((array+((arrayLength-1)/2))+1,arrayLength-((arrayLength-1)/2)+1,value,index+((arrayLength-1)/2)+1);
+}
+
 int main(int argc, char** argv) {
 	int singletonArray[] = {1};
 
 	assert(binarySearch(singletonArray,1,1)==true);
 	assert(binarySearch(singletonArray,1,2)==false);	
 
+	assert(binarySearchWithIndex(singletonArray,1,1,0)==0);
+	assert(binarySearchWithIndex(singletonArray,1,2,0)==-1);	
+	
 	int evenLengthArray[] = {1,2,3,4};
 	
 	assert(binarySearch(evenLengthArray,4,1)==true);
@@ -27,6 +43,12 @@ int main(int argc, char** argv) {
 	assert(binarySearch(evenLengthArray,4,3)==true);
 	assert(binarySearch(evenLengthArray,4,4)==true);	
 	assert(binarySearch(evenLengthArray,4,5)==false);	
+
+	assert(binarySearchWithIndex(evenLengthArray,4,1,0)==0);
+	assert(binarySearchWithIndex(evenLengthArray,4,2,0)==1);	
+	assert(binarySearchWithIndex(evenLengthArray,4,3,0)==2);
+	assert(binarySearchWithIndex(evenLengthArray,4,4,0)==3);	
+	assert(binarySearchWithIndex(evenLengthArray,4,5,0)==-1);	
 
 	int oddLengthArray[] = {1,2,3,4,5};
 
@@ -36,5 +58,18 @@ int main(int argc, char** argv) {
 	assert(binarySearch(oddLengthArray,5,3)==true);	
 	assert(binarySearch(oddLengthArray,5,4)==true);
 	assert(binarySearch(oddLengthArray,5,5)==true);
+
+	assert(binarySearchWithIndex(oddLengthArray,5,0,0)==-1);		
+	assert(binarySearchWithIndex(oddLengthArray,5,1,0)==0);
+
+	printf("Failing case\n");
+	assert(binarySearchWithIndex(oddLengthArray,5,2,0)==1);	
+	assert(binarySearchWithIndex(oddLengthArray,5,3,0)==2);	
+	assert(binarySearchWithIndex(oddLengthArray,5,4,0)==3);
+	assert(binarySearchWithIndex(oddLengthArray,5,5,0)==4);
+	
+	printf("All tests passed\n");
+	
+	return 0;
 }
 
